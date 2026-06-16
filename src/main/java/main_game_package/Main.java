d423b05272ab71a5e920f58452159d6ad1e44505
kiril <wi25b121@technikum-wien.at>
2026-06-16 23:14:11 +0200

    merged Player implementation into staging, initialized game

--- src/main/java/main_game_package/Main.java
+++ src/main/java/main_game_package/Main.java
@@ -1,13 +1,19 @@
 package main_game_package;
 
+import board.Board;
 import game_master.TicTacToe;
+import player.Player;
 
 public class Main {
     static void main() {
 
         try {
 
-            TicTacToe t = new TicTacToe();
+            TicTacToe t = new TicTacToe(
+                    new Player('X'),
+                    new Player('O'),
+                    new Board()
+            );
             t.start();
 
         } catch (Exception e) {
